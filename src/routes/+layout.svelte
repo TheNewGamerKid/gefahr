7f0ebf3acfa4ef52ed20a8f0dd0c18021217b657
Robert Ikerd <robert.ikerd@outlook.com>
2024-04-02 09:47:14 -0400

    Added Some PWA features.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	Notification.requestPermission();
+</script>
+
+<slot />
--- src/service-worker/index.js
+++ src/service-worker/index.js
@@ -79,3 +79,12 @@ self.addEventListener('fetch', (event) => {
 
 	event.respondWith(respond());
 });
+
+self.addEventListener('push', (event) => {
+	event.waitUntil(
+		self.registration.showNotification('Notification Title', {
+			body: 'Notification Body Text',
+			icon: 'custom-notification-icon.png'
+		})
+	);
+});
